55166adc7e576cfd247183cd029cd8822fc4bcbf
donald <donald@leadsoft.co.kr>
2019-09-13 23:27:07 +0900

    first commit

---
+++ src/02-if/2884-AlarmClock.kt
@@ -0,0 +1,15 @@
+package `02-if`
+
+import java.util.*
+
+fun main() {
+    val sc: Scanner = Scanner(System.`in`)
+
+    val hour = sc.nextInt()
+    val minute = sc.nextInt()
+    val underFortyFive: Boolean = minute < 45
+
+    when (hour) {
+
+    }
+}
