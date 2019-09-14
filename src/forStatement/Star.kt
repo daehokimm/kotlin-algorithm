cffb06df1565db6f8cb046a9b0e9053f3f275dbd
donald-dh <rlaeogh222@gmail.com>
2019-09-14 17:59:25 +0900

    repeat()을 이용한 풀이

--- src/forStatement/Star.kt
+++ src/forStatement/Star.kt
@@ -8,15 +8,7 @@ import java.util.*
 fun main() {
     val size = Scanner(System.`in`).nextInt()
 
-    for (i in size downTo 1) {
-        printStar(i, size)
+    for (i in 1..size) {
+        println(" ".repeat(size - i) + "*".repeat(i))
     }
-}
-
-fun printStar(idx: Int, size: Int) {
-    for (i in 1 until idx)
-        print(" ")
-    for (j in idx..size)
-        print("*")
-    println()
 }
