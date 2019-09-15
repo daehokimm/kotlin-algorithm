55f4c10edde60b94c451e48b44f8acd7daea80de
donald-dh <rlaeogh222@gmail.com>
2019-09-15 15:06:51 +0900

    sovled

---
+++ src/forStatement/PlusWhile.kt
@@ -0,0 +1,14 @@
+package forStatement
+
+import java.util.*
+
+/**
+ * 10951
+ */
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    while (sc.hasNext()) {
+        println("${sc.nextInt() + sc.nextInt()}")
+    }
+}
