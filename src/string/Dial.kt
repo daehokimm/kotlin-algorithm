10364fe8fa50551dc4491d57d8e6a387139505ce
donald-dh <rlaeogh222@gmail.com>
2019-10-10 12:50:40 +0900

    refactoring

--- src/string/Dial.kt
+++ src/string/Dial.kt
@@ -4,20 +4,14 @@ package string
 5622
  */
 fun main() {
-    val sum = readLine()!!.map { c -> dial(c) }.sum()
-    print(sum)
+    print("${readLine()!!.map { c -> dial(c) }.sum()}")
 }
 
 fun dial(c: Char) : Int {
-    return when(c) {
-        'A', 'B', 'C' -> 3
-        'D', 'E', 'F' -> 4
-        'G', 'H', 'I' -> 5
-        'J', 'K', 'L' -> 6
-        'M', 'N', 'O' -> 7
-        'P', 'Q', 'R', 'S' -> 8
-        'T', 'U', 'V' -> 9
-        'W', 'X', 'Y', 'Z' -> 10
-        else -> 0
+    return when {
+        c < 'P' -> 3 + (c - 'A') / 3
+        c < 'T' -> 8
+        c < 'W' -> 9
+        else -> 10
     }
 }
